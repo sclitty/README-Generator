f1479cba6ee3fc0956f99146dbe751f693d2f289
Samuel Clitty <samclitty@gmail.com>
2020-09-30 14:46:11 -0700

    basic building files with TODO comments

---
+++ index.js
@@ -0,0 +1,16 @@
+// NPM INIT
+// Install Dependencies - Inquirer
+
+// TODO: require all the dependencies I'll need  - inquirer, fs
+
+// TODO: create an array of questions 
+
+// TODO: writing a README in .md file as template 
+
+// TODO: function that will generate my readme template 
+
+// TODO: use inquirer to prompt users with questions 
+
+// TODO: with that object of user answers from inquirer - pass those into my generate readme function 
+
+// TODO: writeFile using template generated README function 
